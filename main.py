54d7fdda74000def000c1fb86ccd97af5b135791
Hamza Asif <hamza98@live.com.pt>
2021-05-05 19:33:07 +0100

    Added camera livefeed output

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # main.py
-import sys, os
-from PyQt5 import uic
+import sys, os, cv2
+from PyQt5 import QtCore, uic
+from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QMainWindow, QApplication
 
 # Find the correct path of the .ui file
@@ -12,6 +13,30 @@ class MainWindow(QMainWindow, form_class):
         super().__init__()
         # Load the UI
         uic.loadUi("MainWindow.ui", self)
+        
+        self.startCamera.clicked.connect(self.startCapturing)
+        
+    def startCapturing(self):
+        self.camera = cv2.VideoCapture(0)  
+        while self.camera.isOpened():
+            _, image = self.camera.read()
+            self.displayImage(image)
+            cv2.waitKey()
+
+        self.camera.release()
+        cv2.destroyAllWindows()
+        
+    def displayImage(self, img):
+        qformat = QImage.Format_Indexed8 # Find correct image format
+        if len(img.shape) == 3:
+            if img.shape[2] == 4:
+                qformat = QImage.Format_RGBA888
+            else:
+                qformat = QImage.Format_RGB888
+        img = QImage(img, img.shape[1], img.shape[0], qformat) # Create QImage object
+        img = img.rgbSwapped()
+        self.videoFeed.setPixmap(QPixmap.fromImage(img))
+        self.videoFeed.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
         
 if __name__ == "__main__":
     app = QApplication(sys.argv)
