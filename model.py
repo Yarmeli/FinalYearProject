37b72466c19f7c4ed2a8c9889adf75cd40dba4cc
Hamza Asif <hamza98@live.com.pt>
2021-05-09 23:54:06 +0100

    Added method to train the model

--- model.py
+++ model.py
@@ -1,8 +1,8 @@
-import os
+import os, torch
 import pandas as pd
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
 from PIL import Image
 
 class FoodDataset(Dataset):
@@ -58,3 +58,72 @@ class FoodCNN(nn.Module):
         out = self.fc2(out)
         out = self.fc3(out)
         return out
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+ImageClassModel = FoodCNN().to(device)
+
+def train_model():
+    
+    train_csv = "Dataset/train_labels.csv"
+    test_csv = "Dataset/test_labels.csv"
+
+    train_imgdir = "Dataset/Train"
+    test_imgdir = "Dataset/Test"
+    
+    batch_size = 8
+    size = 224
+
+    transform=transforms.Compose([transforms.Resize(size),
+                                  transforms.CenterCrop(size),
+                                  transforms.ToTensor()])
+
+    train_set = FoodDataset(train_csv, train_imgdir, transform=transform)
+    test_set = FoodDataset(test_csv, test_imgdir, transform=transform)
+    
+    train_loader = DataLoader(train_set, batch_size=batch_size)
+    test_loader = DataLoader(test_set, batch_size=batch_size)
+    
+    num_epochs = 5
+    train_losses = []
+    valid_losses = []
+    
+    criterion = nn.CrossEntropyLoss()
+
+    learning_rate = 0.001
+    optimizer = torch.optim.Adam(ImageClassModel.parameters(), lr=learning_rate)
+    
+    for epoch in range(1, num_epochs + 1):
+        train_loss = 0.0
+        valid_loss = 0.0
+        
+        ImageClassModel.train()
+        for data, target in train_loader:
+            
+            data = data.to(device)
+            target = target.to(device)
+            
+            optimizer.zero_grad()
+            output = ImageClassModel(data.float())
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item() * data.size(0)
+        
+        ImageClassModel.eval()
+        for data, target in test_loader:
+            
+            data = data.to(device)
+            target = target.to(device)
+            
+            output = ImageClassModel(data.float())
+            loss = criterion(output, target)
+            
+            valid_loss += loss.item() * data.size(0)
+            
+            train_loss = train_loss/len(train_loader.sampler)
+            valid_loss = valid_loss/len(test_loader.sampler)
+            train_losses.append(train_loss)
+            valid_losses.append(valid_loss)
+                
+        print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(
+            epoch, train_loss, valid_loss))
