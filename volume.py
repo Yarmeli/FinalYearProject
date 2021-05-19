e12565a87bbf0cf0783eab27afb7fcc114d900d7
Hamza Asif <hamza98@live.com.pt>
2021-05-19 02:20:33 +0100

    Added list to keep track of all the classes in the image

--- volume.py
+++ volume.py
@@ -4,6 +4,7 @@ import matplotlib.patches as patches
 from PIL import Image
 
 from helpers import Debug, DebugMode
+from model import output_label
 
 def GetSquareSize(tw_pixel, tw_cm, th_pixel, th_cm):
     # tw_pixel      thumb width in pixels
@@ -49,6 +50,8 @@ def CalculateArea(image, foodItem, thumbvalues):
     
     food_start_x, food_start_y, food_end_x, food_end_y  = None, None, None, None
         
+    allClassesInImage = []
+    
     for y in range(len(image_np)):
         width_thumb = 0
         width_food = 0
@@ -56,6 +59,9 @@ def CalculateArea(image, foodItem, thumbvalues):
         # Scan on the X axis for the width of the thumb
         # Also get the Y axis of where the food starts
         for x in range(len(image_np[y])):
+            if image_np[y][x] not in allClassesInImage:
+                allClassesInImage.append(image_np[y][x])
+            
             if image_np[y][x] == thumbvalue:
                 width_thumb += 1
                 
@@ -75,6 +81,9 @@ def CalculateArea(image, foodItem, thumbvalues):
         # Scan the Y axis for the height of the thumb
         # Also get the X axis of where the food starts
         for y in range(len(image_np)):
+            if image_np[y][x] not in allClassesInImage:
+                allClassesInImage.append(image_np[y][x])
+            
             if image_np[y][x] == thumbvalue:
                 height_thumb += 1
                 
@@ -90,12 +99,19 @@ def CalculateArea(image, foodItem, thumbvalues):
     
     square = GetSquareSize(max_width_thumb, thumbvalues[0], max_height_thumb, thumbvalues[1])
     square = round(square)
+    
+    if square == 0:
+        raise Exception("Unable to find the thumb! Please take another picture")
+    
+    if not any(x in foodItem for x in allClassesInImage):
+        raise Exception(f"Unable to find any of the predicted food! Found this instead: {[output_label(i - 1) for i in allClassesInImage if i != thumbvalue]}")
+    
+    Debug("Volume measurement", f"Food found in this image: {[output_label(i - 1) for i in allClassesInImage if i > 0 and i < thumbvalue]}")
+    
     Debug("Volume measurement", f"Thumb Width values: {max_width_thumb}px, {thumbvalues[0]}cm")
     Debug("Volume measurement", f"Thumb Height values: {max_height_thumb}px, {thumbvalues[1]}cm")
     Debug("Volume measurement", f"Square should be '{square}px' to be equivalent to 1cm2")
     
-    if square == 0:
-        raise Exception("Unable to find the thumb! Please take another picture")
     
    
     BoxStart = [food_start_x - 1, food_start_y - 1] # -1 to not draw over the food item
